303661084600d610b87709c17843973ae931e85e
Ritika Singh Rajput <ritikarajput472@gmail.com>
2026-07-02 11:59:19 +0530

    Time: 3 ms (88.98%) | Memory: 48.4 MB (10.23%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,15 +1,44 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-         int rows = matrix.length;
+        //  int rows = matrix.length;
+        // int cols = matrix[0].length;
+
+        // for(int i=0; i<rows; i++) {
+        //     for(int j=0; j<cols; j++) {
+        //         if(matrix[i][j] == target){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        int rows = matrix.length;
         int cols = matrix[0].length;
 
-        for(int i=0; i<rows; i++) {
-            for(int j=0; j<cols; j++) {
-                if(matrix[i][j] == target){
-                    return true;
-                }
+        // Start from top-right corner
+        int row = 0;
+        int col = cols - 1;
+
+        while (row < rows && col >= 0) {
+
+            int current = matrix[row][col];
+
+            // Case 1: found
+            if (current == target) {
+                return true;
+            }
+
+            // Case 2: current is too big → move left
+            else if (current > target) {
+                col--;
+            }
+
+            // Case 3: current is too small → move down
+            else {
+                row++;
             }
         }
+
         return false;
     }
 }
