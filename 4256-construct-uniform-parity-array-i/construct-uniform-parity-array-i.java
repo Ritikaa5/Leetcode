23bbb7f563d0936ae063f0b5feb2f9e6485bc725
Ritika Singh Rajput <ritikarajput472@gmail.com>
2026-09-02 23:19:23 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (57.87%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // For the easy version (Part I), it is always possible 
+        // to construct a uniform parity array.
+        return true;
+    }
+}
